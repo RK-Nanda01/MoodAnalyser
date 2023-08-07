a31d29f5f62415737ca5f990050df293d30226d7
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-08-07 16:29:33 +0530

    [RK Nanda] Adding a Welcome Message

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyser;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Mood Analyser Problem");
+        Console.ReadLine();
+    }
+}
+
